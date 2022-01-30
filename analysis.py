2f88dc45dd82a2ad87a16eb753b13c175b1af0b3
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-01-30 15:59:50 -0800

    SUNDAY 01.30.2022

--- analysis.py
+++ analysis.py
@@ -1,5 +1,4 @@
 #! /Users/tannerwilliams/Desktop/ME 499/ME499_HW_1_DataAnalysis
-import csv
 
 
 def load_data_from_file(filename):
@@ -30,17 +29,24 @@ def greater_than_index(mylist, mynum):
     :param mynum: specified number
     :return: first instance of a value in list that is greater than or equal to mynum
     """
+    # Check if there are any values in my list larger than my number
     max_mylist = max(mylist)
-    if max_mylist < mynum:
+    # print(max_mylist)
+    if str(max_mylist) < str(mynum):
         return
     else:
+        # There is a larger value than my number...
         for val in mylist:  # Go through all values in the list
-            # Go through list as long as val in mylist is smaller than mynum
-            if val < mynum:
-                continue
-            else:
-                # Return index of that number
-                return print('Position of value in list', mylist.index(val))
+            # Stop when the value is same or larger than my number
+            if val >= mynum:
+                # Return index of said value
+                return mylist.index(val)
+
+            # if val < mynum:
+            #     continue
+            # else:
+            #     # Return index of that number
+            #     return print('Position of value in list', mylist.index(val))
 
 
 def c_initial(filename):
@@ -51,6 +57,7 @@ def c_initial(filename):
     # Get lists from csv file from previous function 'load_data_from_file'
     times, positions = load_data_from_file(filename)
     # return the first index
+    # print('Initial position = ')
     return positions[0]
 
 
@@ -62,6 +69,7 @@ def c_max(filename):
     # Get lists from csv file from previous function 'load_data_from_file'
     times, positions = load_data_from_file(filename)
     # return max of positions
+    # print('Max displacement from initial position = ')
     return max(positions)
 
 
@@ -73,22 +81,73 @@ def c_final(filename):
     # Get lists from csv file from previous function 'load_data_from_file'
     times, positions = load_data_from_file(filename)
     # return the last index of positions
+    # print('Final position of system = ')
     return positions[-1]
 
 
-# def rise_time(filename):
-#     # Get lists from csv file from previous function 'load_data_from_file'
-#     times, positions = load_data_from_file(filename)
+def rise_time(filename):
+    # Get lists from csv file from previous function 'load_data_from_file'
+    times, positions = load_data_from_file(filename)
+    # Retrieve c_initial and c_final... find displacement
+    disp = c_initial(filename) - c_final(filename)
+    # print(disp)
+    # Calculate 10% and 90% values
+    rise_10 = 0.9 * disp
+    rise_90 = 0.1 * disp
+    # Find positions that are closest to these values
+    rise_10_index = greater_than_index(positions, rise_10)
+    # print(rise_10_index)
+    rise_90_index = greater_than_index(positions, rise_90)
+    # print(rise_90_index)
+    # Find corresponding time-stamps and calculate the elapsed time
+    return times[rise_90_index] - times[rise_10_index]
 
 
-if __name__ == '__main__':
-    load_data_from_file('data1.csv')
-    # greater_than_index([1, 3, 4, 7, 10], 6)
-    # greater_than_index([-2.5, 1, 4, 8, 4, 1, -2.5], 4)
-    # greater_than_index([1.1, 2.2, 3.3, 4.4, 5.5], 100.5)
+def peak_time(filename):
+    """
+    :param filename: local .csv file with time vs position
+    :return: time for system to reach max position
+    """
+    # Get lists from csv file from previous function 'load_data_from_file'
+    times, positions = load_data_from_file(filename)
+    # Retrieve c_peak
+    peak = c_max(filename)
+    peak_index = greater_than_index(positions, peak)
+    # print(peak_index)
+    # return time corresponding to the max position or 'peak time'
+    # print('Peak time (s) for system = ')
+    return times[peak_index]
 
-    # print(c_initial('data1.csv'))
-    # print(c_max('data1.csv'))
-    # print(c_final('data1.csv'))
-    #
-    # print(rise_time('data1.csv'))
+
+def perc_overshoot(filename):
+    """
+    :param filename: local .csv file with time vs position
+    :return: percent overshoot of system
+    """
+    # Retrieve c_initial, c_max, and c_final
+    # Distance from initial to final
+    disp = abs(c_initial(filename) - c_final(filename))
+    # Distance from final to max
+    net_disp = abs(c_max(filename) - c_final(filename))
+    # How much did max overshoot the final position
+    return (net_disp / disp) * 100
+
+
+# def settling_time(filename):
+
+
+# if __name__ == '__main__':
+#     # load_data_from_file('data1.csv')
+#     # greater_than_index([1.1, 2.2, 3.3, 4.4, 5.5], 100.5)
+#
+#     print('Initial position = ', c_initial('data1.csv'))
+#     print('Max position = ', c_max('data1.csv'))
+#     print('Final position = ', c_final('data1.csv'))
+#
+#     print('Rise time = ', rise_time('data1.csv'))
+#
+#     print('Peak time = ', peak_time('data1.csv'))
+#
+#     print('Percent overshoot = ', perc_overshoot('data1.csv'))
+#
+#     # print('Settling time (2%) = ', settling_time('data1.csv'))
